5627776604232bc1e7ed685570aa81e91a331748
Anant-219 <anant.dg219@gmail.com>
2024-08-02 23:38:38 +0530

    Added New listing & Updated Home

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const port = '8080'
 const mongo_connection_string = 'mongodb+srv://root:root@cluster0.7hkiyzy.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0'
 
 // Basic SetUP For Express App-----------------------------------------
-app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -32,29 +33,57 @@ main().then((res) => {
 })
 
 const airbnb_data = mongoose.model('airbnb_data', new mongoose.Schema({}, { strict: false }));
+const appData = mongoose.model('test', new mongoose.Schema({}, { strict: false }));
 
 // ----------------------------------------------------------------------------------
 
+
+// Home Route(All Listings)
 app.get('/', async (req, res) => {
 
     all_data = await airbnb_data.find({})
-    // .then((res) => {
-    //     console.log(res);
-    // }).catch((err) => {
-    //     console.log("Errorrr");
-    // })
-    // console.log(user_data)
-    // res.send('working');
+
     res.render('home.ejs', { all_data });
 })
 
-// app.get('/', (req, res) => {
-//     console.log('Root');
-// })
+// Show Route
+app.get('/listing/user/:id', async (req, res) => {
+    let { id } = req.params;
+    // console.log(id);
+    id_data = await airbnb_data.findById(id);
+    console.log(id_data);
 
-// app.get('/', (req, res) => {
-//     console.log('Root');
-// })
+    // console.log(id_data);
+    res.render('edit.ejs', { id_data });
+})
+
+
+
+
+// Create Route
+app.get('/listing/new', (req, res) => {
+    res.render('new.ejs');
+})
+
+app.post('/listing/new/add', (req, res) => {
+    // console.log(req.body);
+    // let { property, city, description, beds, guestsSize, bedrooms, price } = req.body
+
+    const new_app_data = new appData(req.body);
+
+    new_app_data.save()
+        .then((result) => {
+            console.log("Data Added!");
+            res.redirect("/");
+        }).catch((err) => {
+            console.log("Error happend", err);
+        })
+})
+
+
+
+
+
 
 
 app.listen(port, (req, res) => {
